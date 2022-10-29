377e3c55f1dbe400a205c23deeaa920f634de131
Praisehalley <ibehpraiseboma@gmail.com>
2022-10-29 07:31:31 +0100

    changes made to models.py file

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField()
+
+
+class Song(models.Model):
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    date_released = models.DateField()
+    likes = models.PositiveIntegerField()
+
+
+class Lyric(models.Model):
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.TextField()
