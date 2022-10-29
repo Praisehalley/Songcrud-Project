e04b7fbc6a07704bee788e5fe38838281c4cf19e
Praisehalley <ibehpraiseboma@gmail.com>
2022-10-29 08:08:11 +0100

    changes made to models.py file

--- musicapp/models.py
+++ musicapp/models.py
@@ -4,7 +4,10 @@ from django.db import models
 class Artiste(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
-    age = models.IntegerField()
+    age = models.PostiveIntegerField()
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
 
 
 class Song(models.Model):
@@ -12,6 +15,9 @@ class Song(models.Model):
     title = models.CharField(max_length=200)
     date_released = models.DateField()
     likes = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
 
 
 class Lyric(models.Model):
